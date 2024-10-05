8cc5f4bb00cac5b56de5d97a1ef1244cb7934d66
Shivaditya Singh <Shivaditya.Singh@odessainc.com>
2024-10-06 04:36:52 +0530

    UseReducer used

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,48 +1,83 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useReducer } from 'react'
 import axios from 'axios'
+
+const initialState = {
+    weather : null,
+    loading : false,
+    city : '',
+    error : null
+};
+
+const reducer = (state, action) =>
+{
+    switch(action.type)
+    {
+        case 'Weather' : return {...state, weather : action.payload};
+        case 'Loading' : return {...state, loading : action.payload};
+        case 'City' : return {...state, city : action.payload};
+        case 'Error' : return {...state, error : action.payload};
+        default : return state;
+    }
+}
 
 function Weather(){
 
-    const [weather, setWeather] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [city, setCity] = useState('');
-    const [error, setError] = useState(null);
+    // const [weather, setWeather] = useState(null);
+    // const [loading, setLoading] = useState(false);
+    // const [city, setCity] = useState('');
+    // const [error, setError] = useState(null);
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+
 
     useEffect(() => {
         const fetchWeather = async () => {
-            if(!city) return;
 
-            setError(null);
-            setLoading(true);
-            setWeather(null);
+            if(!state.city) return;            
+            // setError(null);
+            // setLoading(true);
+            // setWeather(null);
+
+            dispatch({type : 'Error', payload : null});
+            dispatch({type : 'Loading', payload : true});
+            dispatch({type : 'Weather', payload : null});
+
             try{
-                const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=85cb95fbcede46cfa83214356241807&q=${city}`);
-                setWeather(response.data);
+                const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=85cb95fbcede46cfa83214356241807&q=${state.city}`);
+                //setWeather(response.data);
+                dispatch({type : 'Weather', payload : response.data});
             }
             catch(err){
-                setError(err);
+                //setError(err);
+                dispatch({type : 'Error', payload : err});
+
             }
-            setLoading(false);
+            //setLoading(false);
+            dispatch({type : 'Loading', payload : false});
+
         };
 
         fetchWeather();
-    }, [city]);
+    }, [state.city]);
 
     const handleCityChange = (e) => {
-        setCity(e.target.value);
+        //setCity(e.target.value);
+        dispatch({type : 'City', payload : e.target.value});
+
     };
 
     return (
         <div>
             <h1>Weather-App</h1>
-            <input className='input' type = 'text' value={city} onChange={handleCityChange} placeholder='Enter City Name'/>
-            {loading && <p>Loading..</p>}
-            {error && <p>Error Fetching Data</p>}
-            {weather && (
+            <input className='input' type = 'text' value={state.city} onChange={handleCityChange} placeholder='Enter City Name'/>
+            {state.loading && <p>Loading..</p>}
+            {state.error && !state.weather && <p>Error Fetching Data</p>}
+            {state.weather && (
             <div>
-                <h2>{weather.location.name}</h2>
-                <h3>{weather.current.condition.text}</h3>
-                <h3>{weather.current.temp_c}°C</h3>
+                <h2>City : {state.weather.location.name}</h2>
+                <h3>Condition : {state.weather.current.condition.text}</h3>
+                <h3>Temperature : {state.weather.current.temp_c}°C</h3>
             </div>
         )}
         </div>
